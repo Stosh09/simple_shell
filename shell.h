54ef836c6988acfdfcb24a471fff8854e6203198
OukoFranchez <oukofranchez@gmail.com>
2023-07-21 20:41:55 +0300

    add more function prototypes

--- shell.h
+++ shell.h
@@ -76,4 +76,54 @@ char **get_aliases(char *buffer, char *delimeter);
 int char_found(char c, const char *del);
 char *alias_strtok(char *str, const char *del);
 
+/* m_echo.c files */
+void sub_env(inputs_t *vars);
+void free_av_and_alias(char **s);
+
+/* m_realloc.c files*/
+char **_realloc(char **ptr, size_t old_size, size_t new_size);
+char *_malloc(size_t size);
+
+/* m_process.c file */
+void process_input(char *input, inputs_t *vars);
+int run_command(inputs_t *vars, char *cmd_str);
+char get_operator(char *command, unsigned int *index);
+void check_logical_ops(inputs_t *vars, char *cmd_str);
+
+/* m_builtins.c files */
+int exe_builtin(inputs_t *vars);
+void my_exit(inputs_t *vars);
+void _env(inputs_t *vars);
+void _setenv(inputs_t *vars);
+void _unsetenv(inputs_t *vars);
+
+/* m_path.c files */
+void get_path(inputs_t *vars);
+int execute_cmd(char *command, inputs_t *args);
+
+/* m_print.c files */
+void print_error(inputs_t *vars, char *msg);
+ssize_t _puts(char *str);
+void _puts2(char *str);
+void print_error2(inputs_t *vars, char *msg);
+void print_prompt(void);
+
+/* m_char_conversion.c*/
+char *_uitoa(unsigned int count);
+int _atoi(char *str);
+int _chrcmp(char a, char b);
+
+/* m_cd.c files */
+void _cd(inputs_t *vars);
+void change_dir(inputs_t *args, char *pathname);
+int _setenv2(inputs_t *args, const char *name, const char *val, int o_write);
+char *_getenv(inputs_t *vars, const	char *name);
+
+/* m_alias.c file */
+int check_name_match(char *an_alias, char *input);
+void update_alias(inputs_t *vars, unsigned int k);
+void _alias(inputs_t *vars);
+char *store_alias(char *str);
+void sub_alias(inputs_t *vars);
+
 #endif /*SHELL_H*/
